dc40f4d6cf57027adf5b866f8518dd2ecf926db5
sound-round <yudaev1@gmail.com>
2022-10-29 17:45:25 +0300

    Add brain-prime

---
+++ bin/brain-prime.js
@@ -0,0 +1,25 @@
+#!/usr/bin/env node
+
+import readlineSync from 'readline-sync';
+import { getRandomIntFromInterval, maxNumber, minNumber } from '../src/utils.js';
+import runEngine from '../src/index.js';
+
+const isPrime = (number) => {
+  let divisor = 2;
+  while (number % divisor !== 0) {
+    divisor += 1;
+  }
+  return divisor === number;
+};
+
+const condition = 'Answer "yes" if given number is prime. Otherwise answer "no".';
+
+const playRound = () => {
+  const number = getRandomIntFromInterval(minNumber, maxNumber);
+  console.log(`Question: ${number}`);
+  const userAnswer = readlineSync.question('Your answer?: ');
+  const correctAnswer = isPrime(number) ? 'yes' : 'no';
+  return { correctAnswer, userAnswer };
+};
+
+runEngine(playRound, condition);
